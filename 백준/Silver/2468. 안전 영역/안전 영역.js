ca30e43b032a2a0640c7199d4700d76bd8d81759
JS <80705329+dev-junseo@users.noreply.github.com>
2024-01-23 04:40:12 +0900

    [Silver I] Title: 안전 영역, Time: 324 ms, Memory: 15636 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.js
+++ 백준/Silver/2468. 안전 영역/안전 영역.js
@@ -1,35 +1,35 @@
-const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
-const N = +input[0];
-const areas = input.slice(1).map(v => v.split(' ').map(v => +v));
+const fs = require("fs");
+const filePath =
+    process.platform === "linux" ? "/dev/stdin" : __dirname + "/input.txt";
+const input = fs.readFileSync(filePath, "utf-8").trim().split("\n");
+const N = Number(input.shift())
+const area = input.map(a => a.split(' ').map(Number));
+let ds = [[0, 0, -1, 1], [-1, 1, 0, 0]];
+let maxSafeZone = 0;
 
-const offsetX = [0, 0, -1, 1];
-const offsetY = [-1, 1, 0, 0];
-
-const dfs = (x, y, height, visited) => {
-  offsetX.forEach((dx, i) => {
-    const nx = x + dx;
-    const ny = y + offsetY[i];
-    if (nx >= 0 && nx < N && ny >= 0 && ny < N && !visited[nx][ny]) {
-      visited[nx][ny] = true;
-      dfs(nx, ny, height, visited);
+function dfs(i, j, h, visited) {
+    for (let a = 0; a < 4; a++) {
+        let mx = ds[0][a] + i;
+        let my = ds[1][a] + j;
+        if (my >= 0 && mx >= 0 && my < N && mx < N && !visited[mx][my]) {
+            visited[mx][my] = true;
+            dfs(mx, my, h, visited);
+        }
     }
-  });
-};
-
-let maxCount = 0;
-for (let height = 0; height <= 100; height++) {
-  let count = 0;
-  const visited = [...Array(N)].map((_, x) => [...Array(N)].map((_, y) => areas[x][y] <= height));
-  for (let i = 0; i < N; i++) {
-    for (let j = 0; j < N; j++) {
-      if (!visited[i][j]) {
-        visited[i][j] = true;
-        dfs(i, j, height, visited);
-        count++;
-      }
-    }
-  }
-  maxCount = Math.max(maxCount, count);
 }
 
-console.log(maxCount);
+for (let h = 0; h <= 100; h++) {
+    let safeZone = 0;
+    const visited = [...Array(N)].map((_, x) => [...Array(N)].map((_, y) => area[x][y] <= h));
+    for (let i = 0; i < N; i++) {
+        for (let j = 0; j < N; j++) {
+            if (!visited[i][j]) {
+                visited[i][j] = true;
+                dfs(i, j, h, visited);
+                safeZone++;
+            }
+        }
+    }
+    maxSafeZone = Math.max(maxSafeZone, safeZone);
+}
+console.log(maxSafeZone);
